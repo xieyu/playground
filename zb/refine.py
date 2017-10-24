d044169463beb878cfcdc3fb412275f0444d354a
xie yu <xieyu3@gmail.com>
2017-10-24 17:57:09 +0800

    add zb drafts

---
+++ zb/refine.py
@@ -0,0 +1,9 @@
+bl_info = {
+ "name": "Zero Brush",
+ "author": "Blender Sensei (Seth Fentress) Blendersensei.com",
+ "version": (1, 8, 3),
+ "location": "Properties shelf & Tool shelf of Sensei tab (SPACE: brush menu | Q: options)",
+ "description": "Zero button texture painting by Blender Sensei",
+ "wiki_url": "https://blendersensei.com/zero-brush",
+ "category": "Sensei Format"
+ }
