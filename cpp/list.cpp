a9864ab0c8bdd918062c7a320af09a0ca63ea160
yux <yux@oupeng.com>
2018-02-23 22:22:06 +0800

    draft: finish link list reverse alg

--- cpp/list.cpp
+++ cpp/list.cpp
@@ -1,18 +1,32 @@
 #include <cstddef>
+#include <iostream>
+#include <initializer_list>
+
+using namespace std;
 
 template<typename T>
 class List{
     struct Node {
         T value;
         Node *next;
+        Node():next(nullptr){}
     };
 
 public:
     List():_head(nullptr), _tail(nullptr), _len(0){}
+    List(const std::initializer_list<T>& l);
+    ~List();
+
+    void append(const std::initializer_list<T>& l);
     void append(const T& v);
     void remove(const T& v);
     void remove_at(int index);
+    void revese();
+    void clear();
+    void bubbleSort();
+    void print();
     std::size_t len() const;
+
 private:
     Node* _head;
     Node* _tail;
@@ -20,10 +34,43 @@ private:
 };
 
 template<typename T>
-void List<T>::append(const T& v){
+List<T>::List(const std::initializer_list<T>& values)
+    :_head(nullptr),
+    _tail(nullptr),
+    _len(0)
+{
+    append(values);
+}
+
+template<typename T>
+List<T>::~List(){
+    clear();
+}
+
+template<typename T>
+void List<T>::clear() {
+    Node* iter = _head;
+    while(iter){
+        Node* p = iter;
+        iter = iter->next;
+        delete p;
+    }
+    _head = _tail = nullptr;
+    _len = 0;
+}
+
+template<typename T>
+void List<T>::append(const std::initializer_list<T>& values) {
+    for(const T& i: values) {
+        append(i);
+    }
+}
+
+template<typename T>
+void List<T>::append(const T& v) {
     Node* node = new Node();
     node->value = v;
-    _len += 1;
+    _len++;
 
     if (_head == nullptr) {
         _head = node;
@@ -35,9 +82,63 @@ void List<T>::append(const T& v){
 }
 
 
+template<typename T>
+void List<T>::revese() {
+    if (_head == nullptr) {
+        return;
+    }
+    Node* prev = _head;
+    Node* iter = prev->next;
+    while (iter != nullptr) {
+        Node* next = iter->next;
+        iter->next = prev;
+        prev = iter;
+        iter = next;
+    }
+
+    _head->next = nullptr;
+
+    Node* tmp = _head;
+    _head = _tail;
+    _tail = tmp;
+}
+
+template<typename T>
+void List<T>::print(){
+    Node* iter = _head;
+    while(iter){
+        std::cout<<iter->value<<",";
+        iter = iter->next;
+    }
+    std::cout<<std::endl;
+}
+
+
 int main(){
-    List<int> test;
-    test.append(10);
-    test.append(11);
-    test.append(12);
+    {
+        List<int> test;
+        test.append(10);
+        test.append(11);
+        test.append(12);
+        cout<<"before revese"<<endl;
+        test.print();
+        test.revese();
+        cout<<"after revese"<<endl;
+        test.print();
+    }
+
+    {
+        List<int> test2 = {1, 2, 3, 4, 5};
+        test2.print();
+        test2.revese();
+        test2.print();
+    }
+
+    {
+        List<int> test3{10, 2, 3, 4, 5};
+        test3.print();
+        test3.clear();
+        test3.append({1,4,9,3,2,5,7,0});
+        test3.print();
+    }
 }
