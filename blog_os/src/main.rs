d6889c758e659edd39a78533cb3e4fe64aaacaf6
xieyu <xieyu3@gmail.com>
2019-03-03 18:03:58 +0800

    learn blogos interruption handler

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -21,6 +21,9 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> !{
     println!("hello, world");
     serial_println!("hello Host{}", "!");
+    blog_os::interrupts::init_idt();
+    x86_64::instructions::int3();
+    println!("It's not crash");
     unsafe { exit_qemu();}
     //write!(vga_buffer::WRITER.lock(), "hello").unwrap();
     loop{}
