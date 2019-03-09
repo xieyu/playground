3c6462663eabc46fe79c31d9707f3efa5baa7c1c
Yu Xie <xieyu3@gmail.com>
2019-03-09 18:11:50 +0800

    yew lesson 2

--- rust/hunt-the-wumpus/src/lib.rs
+++ rust/hunt-the-wumpus/src/lib.rs
@@ -1,26 +1,66 @@
 extern crate stdweb;
-
 #[macro_use]
 extern crate yew;
+
+mod components;
+use self::components::controls::Controls;
+use self::components::controls::Stats;
 
 use yew::prelude::*;
 
 pub struct Model {
     arrows: u8,
+    current_room: u8,
+}
+
+fn room_exits(id: u8) -> Option<[u8; 3]> {
+    match id {
+        1 => Some([2, 5, 8]),
+        2 => Some([1, 3, 10]),
+        3 => Some([2, 4, 12]),
+        4 => Some([3, 5, 14]),
+        5 => Some([1, 4, 6]),
+        6 => Some([5, 7, 15]),
+        7 => Some([6, 8, 17]),
+        8 => Some([1, 7, 11]),
+        9 => Some([10, 12, 19]),
+        10 => Some([2, 9, 11]),
+        11 => Some([8, 10, 20]),
+        12 => Some([3, 9, 13]),
+        13 => Some([12, 14, 18]),
+        14 => Some([4, 13, 15]),
+        15 => Some([6, 14, 16]),
+        16 => Some([15, 17, 18]),
+        17 => Some([7, 16, 20]),
+        18 => Some([13, 16, 19]),
+        19 => Some([9, 18, 20]),
+        20 => Some([11, 17, 19]),
+        _ => None,
+    }
 }
 
 #[derive(Debug, Clone)]
-pub enum Msg {}
+pub enum Msg {
+    SwitchRoom(u8),
+}
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Model { arrows: 5 }
+        Model {
+            arrows: 5,
+            current_room: 1,
+        }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        true
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::SwitchRoom(target) => {
+                self.current_room = target;
+                true
+            }
+        }
     }
 }
 
@@ -30,7 +70,8 @@ impl Renderable<Model> for Model {
             <div class="hunt",>
             <div class="header",>{"Hunt the Wumpus"}</div>
             <div class="body",>
-              <span class="arrows",>{&format!("Arrows: {}", self.arrows)}</span>
+                <Stats: arrows=self.arrows, current_room=self.current_room,/>
+                <Controls: exits=room_exits(self.current_room).unwrap(), onsignal=|msg|msg,/>
             </div>
             </div>
         }
